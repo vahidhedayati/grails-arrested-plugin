ff6c80228e719f10d3b7d436b26017fdcd40c881
jotaj90 <jotaj90@hotmailcom>
2014-01-30 16:01:48 -0600

    create a controller work, working on template

--- ArrestedGrailsPlugin.groovy
+++ ArrestedGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ArrestedGrailsPlugin {
     // the plugin version
-    def version = "0.17"
+    def version = "0.187"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
