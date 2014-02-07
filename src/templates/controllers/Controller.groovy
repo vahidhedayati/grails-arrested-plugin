d13271a87488e8c6e9d41ed2832f1c71fb838c9d
jotaj90 <jotaj90@hotmailcom>
2014-02-07 15:24:28 -0600

    ngRoute to angular

--- src/templates/controllers/Controller.groovy
+++ src/templates/controllers/Controller.groovy
@@ -1,51 +1,55 @@
-@package.line@
+@ package
+
+.line @
 
 import grails.converters.JSON
 
-class @controller.name@ {
+class @controller.name @ {
 
     static allowedMethods = [getById: "GET", getAll: "GET", newItem: "POST", update: "PUT", delete: "DELETE"]
 
-    def getById(){
-        render @class.name@.findById(params.id as Long) as JSON
+    def index() {}
+
+    def getById() {
+        render @ class.name @.findById(params.id as Long) as JSON
     }
 
-    def getAll(){
+    def getAll() {
         def areas = []
-        @class.name@.list().each {
+        @ class.name @.list().each {
             areas.add(it)
         }
         render areas as JSON
     }
 
-    def newItem(){
-        def message = [response:"@class.name@_not_created", id:""]
-        if(params.@class.instance@){
-            @class.name@ item = new @class.name@(params.@class.instance@).save(flush: true)
+    def newItem() {
+        def message = [response: "@class.name@_not_created", id: ""]
+        if (params.@class.instance @) {
+            @ class.name @item = new @ class.name @(params.@class.instance @).save(flush: true)
             message.response = "@class.name@_created"
             message.id = item.id
         }
         render message as JSON
     }
 
-    def update(){
-        def message = [response:"@class.name@_not_updated"]
-        if(params.@class.instance@){
-            @class.name@ item = @class.name@.findById(params.@class.instance@.id as Long)
-            if(item){
-                    item.properties = params.@class.instance@
-                    item.save(flush: true)
-                    message.response = "@class.name@_updated"
+    def update() {
+        def message = [response: "@class.name@_not_updated"]
+        if (params.@class.instance @) {
+            @ class.name @item = @ class.name @.findById(params.@class.instance @.id as Long)
+            if (item) {
+                item.properties = params.@class.instance @
+                item.save(flush: true)
+                message.response = "@class.name@_updated"
 
             }
         }
         render message as JSON
     }
 
-    def delete(){
-        def message = [response:"@class.name@_not_deleted"]
-        @class.name@ item = @class.name@.findById(params.id as Long)
-        if(item){
+    def delete() {
+        def message = [response: "@class.name@_not_deleted"]
+        @ class.name @item = @ class.name @.findById(params.id as Long)
+        if (item) {
 
             item.delete(flush: true)
             message.response = "@class.name@_deleted"
