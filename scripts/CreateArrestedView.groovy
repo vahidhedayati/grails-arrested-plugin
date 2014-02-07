319a338fd8cfc3b393f6f9c5dfb1d681a7f8db22
Marlon Rojas <marlon.rojas@puresrc.com>
2014-02-07 13:28:02 -0600

    create-view updated

--- scripts/CreateArrestedView.groovy
+++ scripts/CreateArrestedView.groovy
@@ -8,4 +8,5 @@ USAGE = """
 target(default: "This creates the GSP mapped at index (view is index.gsp) AND the javascript controller") {
     depends(parseArguments, createViewController)
     depends(parseArguments, createJSController)
+    depends(parseArguments, createAngularIndex)
 }
