8957ad1525a0488592f99ad1f60d050fc196e91e
Vahid <vahid.hedayati@gmail.com>
2014-05-05 20:57:59 +0100

    ui updates 1.15

--- scripts/_ArrestedInternal.groovy
+++ scripts/_ArrestedInternal.groovy
@@ -352,10 +352,12 @@ target(createAngularIndex: "Create the angular file configuration") {
 		writer.writeLine "            when('/signup', {templateUrl: '/" + Metadata.current.'app.name' + "/auth/showSignup', controller: 'UserCtrl'})."
 		writer.writeLine "            when('/updateinfo', {templateUrl: '/" + Metadata.current.'app.name' + "/auth/showUpdateInfo', controller: 'UserCtrl'})."
         names.each {
-            writer.writeLine "            when('/" + it.propertyName + "/create', {templateUrl: '/" + Metadata.current.'app.name' + "/" + it.propertyName + "/edit', controller: '" + it.className + "Ctrl'})."
-            writer.writeLine "            when('/" + it.propertyName + "/edit', {templateUrl: '/" + Metadata.current.'app.name' + "/" + it.propertyName + "/edit', controller: '" + it.className + "Ctrl'})."
-            writer.writeLine "            when('/" + it.propertyName + "/list', {templateUrl: '/" + Metadata.current.'app.name' + "/"  + it.propertyName + "/listing', controller: '" + it.className + "Ctrl'})."
-            writer.writeLine "            when('/" + it.propertyName + "', {templateUrl: '/" + Metadata.current.'app.name' + "/" + it.propertyName + "/listing', controller: '" + it.className + "Ctrl'})."
+			if (new File("${basedir}/web-app/js/${it.className}Ctrl.js").exists()) {
+				writer.writeLine "            when('/" + it.propertyName + "/create', {templateUrl: '/" + Metadata.current.'app.name' + "/" + it.propertyName + "/edit', controller: '" + it.className + "Ctrl'})."
+				writer.writeLine "            when('/" + it.propertyName + "/edit', {templateUrl: '/" + Metadata.current.'app.name' + "/" + it.propertyName + "/edit', controller: '" + it.className + "Ctrl'})."
+				writer.writeLine "            when('/" + it.propertyName + "/list', {templateUrl: '/" + Metadata.current.'app.name' + "/"  + it.propertyName + "/listing', controller: '" + it.className + "Ctrl'})."
+				writer.writeLine "            when('/" + it.propertyName + "', {templateUrl: '/" + Metadata.current.'app.name' + "/" + it.propertyName + "/listing', controller: '" + it.className + "Ctrl'})."
+			}	
         }
         writer.writeLine "            otherwise({redirectTo: '/login'});"
         writer.writeLine "    }"
@@ -686,44 +688,47 @@ target(updateLayout: "Update the layout view") {
     configFile.createNewFile()
     configFile.withWriterAppend { BufferedWriter writer ->
         writer.writeLine """
-			<div class="container row" data-ng-controller="UserCtrl" data-ng-show="appConfig.token!=''">
-                   <div class="col-md-12">
-                    <p></p>
-                    <ul class="nav navbar-nav col-md-12" style="min-height: 30px;">
-						<li class='controller'>
-							<a class="dropdown-toggle" role="button" data-toggle="dropdown" data-target="#" href="#" id="userBox">
-							<i class="icon-user icon-white"></i>
-							<span id="userMessage">
-								<span class="glyphicon glyphicon-user"></span>
-								<g:message code="default.user.label" default="{{user.username}}" />
-							</span>
-					 		<b class="caret"></b>
-							</a>
-							<ul class="dropdown-menu" role="menu"  id="authBox">
-							<li>
-								<a onclick='window.location.href="#/updateinfo"' title="\${message(code: 'default.userdetails.update', default: 'Update info')}">
-								<g:message code="default.userdetails.update"  default="Update info"/>	
-                                </a>
-							</li>
-							</ul>
-							</li>
-							<g:each var="c" in="\${grailsApplication.controllerClasses.sort { it.fullName } }">
-                             	<g:if test="\${!(c.fullName.contains('DbdocController')||c.fullName.contains('ArrestedUser')||c.fullName.contains('ArrestedController')||c.fullName.contains('AuthController'))}">
-                                    <li class="controller">
-                                        <a onclick='window.location.href="#/\${c.logicalPropertyName}/list"' title="\${message(code: 'default.'+c.name+'.update', default: ''+c.name+'')}">
-											<g:message code="default.\${c.name}.label"  default="\${c.name}"/>	
-                                        </a>
-                                    </li>
-                                </g:if>
-                            </g:each>
-					 		<li class='controller'>
-								<a data-ng-controller='UserCtrl' data-ng-click='logout()' title="\${message(code: 'security.signoff.label', default: 'Log out')}">
-								<span class="glyphicon glyphicon-log-out"></span> <g:message code="security.signoff.label" default="Sign Off"/>
-							</a>
-							</li>
-						</ul>
-					</div>
-				</div>
+	<nav class="navbar navbar-default" role="navigation" data-ng-show="appConfig.token!=''">
+	<div class="container-fluid" data-ng-controller="UserCtrl" >
+	
+    	<ul  class="nav navbar-left" >
+			<g:each var="c" in="\${grailsApplication.controllerClasses.sort { it.fullName } }">
+            	<g:if test="\${!(c.fullName.contains('DbdocController')||c.fullName.contains('ArrestedUser')||c.fullName.contains('ArrestedController')||c.fullName.contains('AuthController'))}">
+                	<li class="controller">
+                    	<a onclick='window.location.href="#/\${c.logicalPropertyName}/list"' title="\${message(code: 'default.'+c.name+'.update', default: ''+c.name+'')}">
+							<g:message code="default.\${c.name}.label"  default="\${c.name}"/>	
+                        </a>
+                     </li>
+                  </g:if>
+              </g:each>
+         </ul>
+         
+         <ul  class="nav navbar-right" >
+         	<li  class="dropdown">
+				<a class="dropdown-toggle" role="button" data-toggle="dropdown">
+					<span id="userMessage">
+						<span class="glyphicon glyphicon-user"></span>
+							<g:message code="default.user.label" default="{{user.username}}" />
+						</span>
+					 	<b class="caret"></b>
+				</a>
+				<ul class="dropdown-menu" role="menu">
+					<li >
+						<a onclick='window.location.href="#/updateinfo"' title="\${message(code: 'default.userdetails.update', default: 'Update info')}">
+							<g:message code="default.userdetails.update"  default="Update info"/>	
+                        </a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a data-ng-controller='UserCtrl' data-ng-click='logout()' title="\${message(code: 'security.signoff.label', default: 'Log out')}">
+					<span class="glyphicon glyphicon-log-out"></span> <g:message code="security.signoff.label" default="Sign Off"/>
+				</a>
+			</li>
+		</ul>
+		
+	</div>
+</nav>
 """
 }
     depends(compile)
