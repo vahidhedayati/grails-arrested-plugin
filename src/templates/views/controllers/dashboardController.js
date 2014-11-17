383ddac9a2f7f72d443238a723b3bb75e8d136fb
vahid <vahid.hedayati@gmail.com>
2014-11-17 14:05:14 +0000

    updates to dashboard

--- src/templates/views/controllers/dashboardController.js
+++ src/templates/views/controllers/dashboardController.js
@@ -1,18 +1,24 @@
 'use strict';
 
 function DashboardCtrl(\$cookieStore,\$scope,\$rootScope,LangService,\$cacheFactory,\$templateCache) {
-
-	\$rootScope.appConfig = {appName:'$appName', token:''}; 
-	\$rootScope.user = {id:'', username:'', passwordHash:''}; 
-	if(\$cookieStore.get('token')) { 
-		\$rootScope.appConfig.token = \$cookieStore.get('token'); 
-		\$rootScope.user.id = \$cookieStore.get('id'); 
-		\$rootScope.user.username = \$cookieStore.get('username'); 
-	}
 	
     \$scope.dashboard=function() { 
+    	/*
     	\$templateCache.removeAll();
     	\$cacheFactory.get('\$http').removeAll();
+    	*/
+
+
+    	\$rootScope.appConfig = {appName:'$appName', token:''}; 
+    	\$rootScope.user = {id:'', username:'', passwordHash:''}; 
+    	if(\$cookieStore.get('token')) { 
+    		\$rootScope.appConfig.token = \$cookieStore.get('token'); 
+    		\$rootScope.user.id = \$cookieStore.get('id'); 
+    		\$rootScope.user.username = \$cookieStore.get('username'); 
+    	}
+
+    	
+    	
     	LangService.getLang()
     	.then(function(data) {
 		  \$rootScope.myLang = data;
